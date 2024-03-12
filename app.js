c15c37e646f5aaa8c1dfd91709272e9be0d127bb
Myintzu Thinn <myintzuthinn91@gmail.com>
2024-03-13 01:06:02 +0630

    visa card 13/march/2024 01:05

---
+++ app.js
@@ -0,0 +1,28 @@
+// Get UI
+const inputs = document.querySelectorAll('input');
+// console.log(inputs);
+let datas =[];
+
+inputs.forEach(function(input){
+	// console.log(input);
+
+	input.addEventListener('keyup',function(){
+		// console.log(this.value);
+
+
+		datas.push({
+			digit:inputs[0].value,
+			name:inputs[1].value,
+			exp:inputs[2].value,
+			ccv:inputs[3].value
+		});
+
+		console.log(datas[datas.length-1]);
+	});
+});
+
+
+// 8CV
+
+
+ 
